483df9dc2cef1e7090e25141294e52e7a0cee5de
ingrid <ferreiraingrid33@gmail.com>
2021-01-24 14:10:13 -0300

    Create Notas.py

---
+++ Notas.py
@@ -0,0 +1,8 @@
+nota1 = int(input("Insira a nota: "))
+nota2 = int(input("Insira a nota: "))
+nota3 = int(input("Insira a nota: "))
+nota4 = int(input("Insira a nota: "))
+
+m = (nota1+nota2+nota3+nota4)/4
+
+print("1° NOTA: {} \n 2° NOTA: {} \n 3° NOTA: {} \n 4° NOTA: {} \n Sua média é: {}".format(nota1,nota2,nota3,nota4,m))
